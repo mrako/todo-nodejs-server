26ff43e7d9b1d542c77e4bf05dbecb07c06e78e1
Marko Klemetti <marko.klemetti@gmail.com>
2014-08-08 11:05:47 +0300

    Updated CORS

--- app/cors.js
+++ app/cors.js
@@ -1,5 +1,7 @@
 module.exports = function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  
   next();
 }
